836297376187611948aaf7ccecb3a6ae606adc1f
Shinpanda <duldulgo1@gmail.com>
2023-04-04 13:51:29 +0900

    🎨  root createRoot로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./App";
 
 const queryClient = new QueryClient();
-ReactDOM.render(
+
+const rootDom = document.getElementById("root");
+if (!rootDom) throw new Error("root가 없습니다.");
+const root = ReactDOM.createRoot(rootDom);
+
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
